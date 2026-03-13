498461e0bf367f805f46212faaad3adcf06a95ba
Benjamin McEwan <benrobmc.mc@gmail.com>
2026-03-13 19:51:27 +0000

    backup

--- main.c
+++ main.c
@@ -11,30 +11,20 @@ const int FONT_ID_BODY_16 = 0;
 
 const Clay_Color COLOUR_WHITE          = { 245, 245, 245, 255 };
 const Clay_Color COLOUR_BACKGROUND     = { 36, 36, 36, 255 };
+const Clay_Color COLOUR_PANEL          = { 56, 56, 56, 255 };
+const Clay_Color COLOUR_PANEL2         = { 76, 76, 76, 255 };
 
+const Clay_Color COLOUR_POSITIVE       = { 142, 240, 115, 255 };
+const Clay_Color COLOUR_NEGATIVE       = { 240, 115, 115, 255 };
 
-// LAYOUT ------------------------------------------------------------------------------------------
-Clay_RenderCommandArray layoutMain() {
-    Clay_BeginLayout(); // BEGIN LAYOUT
-
-    CLAY(CLAY_ID("containerMain"), {
-        .backgroundColor = COLOUR_BACKGROUND,
-        .layout = {
-            .layoutDirection = CLAY_TOP_TO_BOTTOM,
-            .sizing = {
-                .width = CLAY_SIZING_GROW(),
-                .height = CLAY_SIZING_GROW()
-            },
-            .childGap = 8
-        }
-    }) {
-
-    }
-
-    // END LAYOUT
-    Clay_RenderCommandArray renderCommands = Clay_EndLayout();
-    return renderCommands;
+// utilities
+Clay_String utilFixedClayString(char *text) {
+    return (Clay_String) {
+        .length = (int)strlen(text),
+        .chars = text
+    };
 }
+
 
 // JSON PARSER -------------------------------------------------------------------------------------
 cJSON* ParseFileJSON(char filepath[]) {
@@ -72,6 +62,92 @@ cJSON* ParseFileJSON(char filepath[]) {
 
     free(data);
     return json;
+}
+
+
+// LAYOUT ------------------------------------------------------------------------------------------
+void layoutTransaction(cJSON *transaction) {
+    cJSON *account = cJSON_GetObjectItem(transaction, "account");
+    cJSON *payee = cJSON_GetObjectItem(transaction, "payee");
+    cJSON *currency = cJSON_GetObjectItem(transaction, "currency");
+        // amount
+        // tags
+    cJSON *timestamp = cJSON_GetObjectItem(transaction, "timestamp");
+
+    CLAY_AUTO_ID({
+        .backgroundColor = COLOUR_PANEL2,
+        .layout = {
+            .sizing = {
+                .width = CLAY_SIZING_GROW(),
+                .height = CLAY_SIZING_FIXED(45)
+            },
+            .childGap = 16
+        }
+    }) {
+        CLAY_TEXT(utilFixedClayString(currency->valuestring), CLAY_TEXT_CONFIG({
+            .fontId = FONT_ID_BODY_16,
+            .fontSize = 16,
+            .textColor = COLOUR_WHITE
+        }));
+
+        CLAY_TEXT(utilFixedClayString(account->valuestring), CLAY_TEXT_CONFIG({
+            .fontId = FONT_ID_BODY_16,
+            .fontSize = 16,
+            .textColor = COLOUR_WHITE
+        }));
+
+        CLAY_TEXT(utilFixedClayString(payee->valuestring), CLAY_TEXT_CONFIG({
+            .fontId = FONT_ID_BODY_16,
+            .fontSize = 16,
+            .textColor = COLOUR_WHITE
+        }));
+
+        // amount
+
+        CLAY_TEXT(utilFixedClayString(timestamp->valuestring), CLAY_TEXT_CONFIG({
+            .fontId = FONT_ID_BODY_16,
+            .fontSize = 16,
+            .textColor = COLOUR_WHITE
+        }));
+    }
+}
+
+Clay_RenderCommandArray layoutMain(cJSON *json_transactions) {
+    Clay_BeginLayout(); // BEGIN LAYOUT
+
+    CLAY(CLAY_ID("containerMain"), {
+        .backgroundColor = COLOUR_BACKGROUND,
+        .layout = {
+            .layoutDirection = CLAY_TOP_TO_BOTTOM,
+            .sizing = {
+                .width = CLAY_SIZING_GROW(),
+                .height = CLAY_SIZING_GROW()
+            },
+            .childGap = 8
+        }
+    }) {
+        CLAY(CLAY_ID("containerTransactions"), {
+            .backgroundColor = COLOUR_PANEL,
+            .layout = {
+                .layoutDirection = CLAY_TOP_TO_BOTTOM,
+                .sizing = {
+                    .width = CLAY_SIZING_PERCENT(0.7),
+                    .height = CLAY_SIZING_GROW()
+                },
+                .childGap = 2
+            }
+        }) {
+            int transaction_count = cJSON_GetArraySize(json_transactions);
+            for(int i = 0; i < transaction_count; i++){
+                cJSON *transaction = cJSON_GetArrayItem(json_transactions, i);
+                layoutTransaction(transaction);
+            }
+        }
+    }
+
+    // END LAYOUT
+    Clay_RenderCommandArray renderCommands = Clay_EndLayout();
+    return renderCommands;
 }
 
 
@@ -134,7 +210,7 @@ int main(void) {
         SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 
         // get layout render commands for the ui
-        Clay_RenderCommandArray renderCommands = layoutMain();
+        Clay_RenderCommandArray renderCommands = layoutMain(json_transactions);
 
         // draw the ui
         BeginDrawing();
